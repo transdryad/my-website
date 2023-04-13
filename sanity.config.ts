9afbbb36f7a0432bde0dbd6003f4d3d124828086
sisusam <viswanathsam@gmail.com>
2023-04-13 16:16:45 -0600

    schema type project

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "sanity";
 import { deskTool } from 'sanity/desk';
+import project from "./sanity/schemas/project-schema";
 
 const config = defineConfig({
   projectId: 'l5cflhi9',
@@ -7,7 +8,8 @@ const config = defineConfig({
   title: 'My Personal Wesite',
   apiVersion: '2023-4-12',
   basePath: '/admin',
-  plugins: [deskTool()]
+  plugins: [deskTool()],
+  schema: { types: [project] }
 })
 
 export default config;

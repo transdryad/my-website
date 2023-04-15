90e773716692c47ff1ae09284143aeb30ae614b9
sisusam <viswanathsam@gmail.com>
2023-04-15 09:44:26 -0600

    minor formatting

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,12 @@ import { getProjects } from "@/sanity/sanity-utils";
 
 export default async function Home() {
   const projects = await getProjects();
-  return <div>
+
+  return (
+  <div>
     {projects.map((project) => (
       <div key={project._id}>{project.name}</div>
     ))}
-  </div>;
+  </div>
+  ); 
 }

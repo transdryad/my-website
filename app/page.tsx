969c01e38ce561c9a1ee1de5baf95f3182cfb640
sisusam <viswanathsam@gmail.com>
2023-05-02 18:45:29 -0600

    fixed gradient

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,12 @@ export default async function Home() {
 
   return (
   <div className='max-w-5xl mx-auto py-20'>
-    <h1 className='text-7xl font-extrabold'>Hello I&apos;m <span className='bg-gradient-to-r 
-    from-orange-500 via-red-500 to-purple-600'>Hazel</span></h1>
+    <h1 className='text-7xl font-extrabold'>
+      Hello I&apos;m{' '}
+      <span className='bg-gradient-to-r from-orange-500 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+        Hazel
+      </span>
+    </h1>
 
     {projects.map((project) => (
       <div key={project._id}>

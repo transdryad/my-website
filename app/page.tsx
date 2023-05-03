5c246dacae20c485ee4f2b3b749238f30bf041e7
sisusam <viswanathsam@gmail.com>
2023-05-02 18:40:10 -0600

    began styling

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,10 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-  <div className='bg-red-500'>
+  <div className='max-w-5xl mx-auto py-20'>
+    <h1 className='text-7xl font-extrabold'>Hello I&apos;m <span className='bg-gradient-to-r 
+    from-orange-500 via-red-500 to-purple-600'>Hazel</span></h1>
+
     {projects.map((project) => (
       <div key={project._id}>
         {project.name}

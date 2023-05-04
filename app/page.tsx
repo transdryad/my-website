494753c795a5b0022b46d8ed946ef2ea88adc0f3
sisusam <viswanathsam@gmail.com>
2023-05-04 06:13:01 -0600

    continues styling 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { getProjects } from "@/sanity/sanity-utils";
 import Image from "next/image";
+import Link from 'next/link';
 
 export default async function Home() {
   const projects = await getProjects();
@@ -21,20 +22,20 @@ export default async function Home() {
 
 <div className='mt-5 grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
     {projects.map((project) => (
-      <div key={project._id} className='border border-gray-500 rounded-lg p-3'>
+      <Link href={'/projects/${project.slug}'} key={project._id} className='border-2 border-gray-500 rounded-lg p-1'>
         {project.image &&(
           <Image
             src={project.image}
             alt={project.name}
-            width={250}
-            height={100}
+            width={750}
+            height={300}
             className='object-cover rounded-lg border border-gray-500'
           />
         )}
-        <div className='font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
+        <div className='mt-2 font-extrabold bg-gradient-to-r from-orange-400 via-red-500 to-purple-600 bg-clip-text text-transparent'>
           {project.name}
         </div>
-      </div>
+      </Link>
     ))}
 </div>
   </div>

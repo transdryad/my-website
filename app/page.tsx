2f7838c89730374fb716e33d6fba116909dc016d
sisusam <viswanathsam@gmail.com>
2023-05-06 16:19:34 -0600

    styling2

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default async function Home() {
   const projects = await getProjects();
 
   return (
-  <div className='max-w-5xl mx-auto py-20'>
+  <div>
     <h1 className='text-7xl font-extrabold'>
       Hi, I&#8217;m{' '}
       <span className='bg-gradient-to-r from-blue-400 via-teal-500 to-green-600 bg-clip-text text-transparent'>
@@ -20,9 +20,9 @@ export default async function Home() {
 
     <h2 className='mt-24 font-bold text-gray-700 text-3xl'>My Projects</h2>
 
-<div className='mt-5 grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+    <div className='mt-5 grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
     {projects.map((project) => (
-      <Link href={`/projects/${project.slug}`} key={project._id} className='border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-400 transition'>
+      <Link href={`/projects/${project.slug}`} key={project._id} className='border-2 border-gray-500 rounded-lg p-1 hover:scale-105 hover:border-blue-500 transition'>
         {project.image &&(
           <Image
             src={project.image}
--- app/projects/[project]/page.tsx
+++ app/projects/[project]/page.tsx
@@ -10,7 +10,7 @@ export default async function Project({ params }: Props) {
     const slug = params.project;
     const project = await getProject(slug);
 
-    return <div className='max-w-3xl mx-auto py-20'>
+    return <div>
         <header className='flex items-center justify-between'>
             <h1 className='bg-gradient-to-r from-blue-400 via-teal-500 to-green-600 bg-clip-text text-transparent text-5xl drop-shadow font-extrabold'>{project.name}</h1>
 
